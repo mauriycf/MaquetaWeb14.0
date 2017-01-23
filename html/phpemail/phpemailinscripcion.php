c7bd1998de41ca62626c020fc871a0cbf3881fd0
mauriycf <mauriycf@gmail.com>
2017-01-23 08:20:17 -0430

    23/01/2017

---
+++ html/phpemail/phpemailinscripcion.php
@@ -0,0 +1,76 @@
+<?php
+
+  require 'vendor/autoload.php';
+
+  $mail = new PHPMailer;
+
+  $mail->isSMTP();
+  $mail->Host = 'smtp.gmail.com';
+  $mail->SMTPAuth = true;
+  $mail->Username = 'oticmatvenezuelaPhpmailer@gmail.com';
+  $mail->Password = 'svhloq0yokn0';
+  $mail->SMTPSecure = 'ssl';
+  $mail->Port = 465;
+  // $mail->SMTPDebug = 3;
+
+  $nombre=$_POST['nombre'];
+  $apellido=$_POST['apellido'];
+  $cedula = $_POST['cedula'];
+  $pasaporte = $_POST['pasaporte'];
+  $fecha_nac = $_POST['fecha_nac'];
+  $lugar = $_POST['lugar'];
+  $email = $_POST['email'];
+  $telefonoHabitacion = $_POST['telefonoHabitacion'];
+  $telefonoCelular = $_POST['telefonoCelular'];
+  $domicilio = $_POST['domicilio'];
+  $estado = $_POST['estado'];
+  $municipio = $_POST['municipio'];
+  $parroquia = $_POST['parroquia'];
+  $tituloObtenido = $_POST['tituloObtenido'];
+  $carrera = $_POST['carrera'];
+  $institucion = $_POST['institucion'];
+  $profesion = $_POST['profesion'];
+  $nombreEmpresa = $_POST['nombreEmpresa'];
+  $direccionTrabajo = $_POST['direccionTrabajo'];
+
+  $error;
+    $address = $email;
+
+    $mail->setFrom($address , 'Datos de Inscripcion: ' . $nombre . ' ' . $apellido . ' Correo: ' . $email );
+    $mail->addAddress($address, 'oticmatvenezuela@gmail.com');
+
+    $mail->isHTML( true);
+
+    $mail->Subject = 'Solicitud de inscripcion';
+    $mail->Body = 'Solicitud de inscripción para Diplomado en Teledetección
+    y Sistemas de Información Geográfica aplicado a la Agricultura <br>' . 'Enviado por: ' . $nombre . ' ' . $apellido . '<br>' .
+    'Los siguientes datos: <br><br> Cedula: ' . $cedula . '<br> Pasaporte: ' . $pasaporte . '<br> Fecha de Nacimiento: ' . $fecha_nac .
+    '<br> Lugar de nacimiento: ' . $lugar . '<br> email: ' . $email . '<br> Telefono Habitacion: ' . $telefonoHabitacion . '<br> Telefono Celular: ' . $telefonoCelular .
+    '<br> Domicilio: ' . $domicilio . '<br> Estado: ' . $estado . '<br> Municipio: ' . $municipio . '<br> Parroquia: ' . $parroquia . '<br> Titulo Obtenido: ' . $tituloObtenido .
+    '<br> Carrera: ' . $carrera . '<br> Institucion: ' . $institucion . '<br> Profesion: ' . $profesion . '<br> Nombre de la Empresa: ' . $nombreEmpresa . '<br> Direccion de trabajo: ' . $direccionTrabajo .
+    '<br><br> Datos llenos para la inscripcion.';
+    $mail->AltBody = 'OTIC';
+
+    if (!$mail->send()) {
+      echo 'El mensaje no pudo ser enviado <br>';
+      echo "Error de phpMailer " . $mail->ErrorInfo;
+    }else{
+      $msgExitoso = "La inscripción fue enviada, por favor espere 24 horas para seguir con la inscripcion";
+    }
+  // }
+
+  ?>
+
+  <?php include 'html/overall/header.html'; ?>
+
+  <?php include 'html/overall/top-nav.html'; ?>
+
+  <section class="main container">
+    <div class="row">
+      <article class="post clearfix">
+        <?php echo $msgExitoso,$error; echo 'Go to: <a href="?view=index"><strong>Inicio<strong></a>'?>
+      </article>
+    </div>
+  </section>
+
+  <?php include 'html/overall/footer.html'; ?>
--- html/registro-prueba/registro.php
+++ html/registro-prueba/registro.php
@@ -2,9 +2,9 @@
 
 <?php include 'html/overall/top-nav.html'; ?>
 
-<?php include 'html/registro-prueba/functions.php'; ?>
+<!-- <?php include 'html/registro-prueba/functions.php'; ?>
 <?php include 'html/registro-prueba/municipios.php'; ?>
-<?php include 'html/registro-prueba/parroquias.php'; ?>
+<?php include 'html/registro-prueba/parroquias.php'; ?> -->
 
 
 <style>
@@ -20,23 +20,23 @@ display:block;
 <div class="panel panel-default">
     <div class="panel-body">
       <h3 class="info">¡REGISTRO!</h3>
-      <form action="?view=registropresona"method="POST">
+      <form action="?view=phpemailinscripcion"method="POST">
             <div class="modal-body">
                 <div class="form">
                     <div class="form-group">
                         <label for="nombre"><span class="glyphicon glyphicon-user"></span> Nombres</label>
-                        <input type="text" class="form-control" name="nombre" id="user_login" placeholder="Introducir Nombres">
+                        <input type="text" class="form-control" name="nombre" id="user_login" placeholder="Introducir Nombres" required>
                     </div>
                     <div class="form-group">
                         <label for="apellido"><span class="glyphicon glyphicon-user"></span> Apellidos</label>
-                        <input type="text" class="form-control" name="apellido" id="user_login" placeholder="Introducir Apellidos">
+                        <input type="text" class="form-control" name="apellido" id="user_login" placeholder="Introducir Apellidos" required>
                     </div>
                     <div class="form-group">
                         <label for="cedula"><span class="glyphicon glyphicon-lock"></span> Nacionalidad</label>
-                        <select class="form-control" id="sel1" name="cedula">
+                        <select class="form-control" id="sel1" name="cedula" required>
                           <option value="0">-</option>
-                          <option value="1">V</option>
-                          <option value="2">E</option>
+                          <option value="V">V</option>
+                          <option value="E">E</option>
                         </select>
                     </div>
                     <div class="form-group">
@@ -46,65 +46,56 @@ display:block;
                     <div class="form-group">
                         <label for="fechaNacimiento"><span class="glyphicon glyphicon-user"></span> Lugar y Fecha de Nacimiento</label><br>
                         <input type="date" placeholder="dia-mes-año" name="fecha_nac" required><br>
-                        <input type="text" class="form-control" name="lugar" id="fechaNacimiento" placeholder="Lugar">
+                        <input type="text" class="form-control" name="lugar" id="fechaNacimiento" placeholder="Lugar" required>
                     </div>
                     <div class="form-group">
                         <label for="correoElectronico"><span class="glyphicon glyphicon-user"></span> Correo Electrónico</label>
-                        <input type="text" class="form-control" name="email" id="email" placeholder="E-mail">
+                        <input type="text" class="form-control" name="email" id="email" placeholder="E-mail" required>
                     </div>
                     <div class="form-group">
                         <label for="telefono"><span class="glyphicon glyphicon-user"></span> Teléfonos</label>
-                        <input type="text" class="form-control" name="telefonoHabitacion" id="telefonoHabitacion" placeholder="Habitacion">
-                        <input type="text" class="form-control" name="telefonoCelular" id="telefonoCelular" placeholder="Celular" >
+                        <input type="text" class="form-control" name="telefonoHabitacion" id="telefonoHabitacion" placeholder="Habitacion" >
+                        <input type="text" class="form-control" name="telefonoCelular" id="telefonoCelular" placeholder="Celular"  required>
                     </div>
-                    <div class="form-group">
+                   <div class="form-group">
                         <label for="domicilio"><span class="glyphicon glyphicon-user"></span> Domicilio Completo</label>
                         <input type="text" class="form-control" name="domicilio" id="domicilio" placeholder="Indique(calle,avenida,edificio,piso etc)">
                         <label for="domicilio"> Estado</label>
-                        <select id="estados" class="form form-control" name="estados">
-                           <option></option>
-                         <?php __ListarEstados(); ?>
-                        </select>
+                        <input type="text" name="estado" required>
                         <label for="domicilio"> Municipio</label>
-                        <select id="municipios" class="form form-control" name="municipios">
-                          <option></option>
-                          <?php __ListarMunicipios(); ?>
-                        </select>
+                        <input type="text" name="municipio" required>
                         <label for="domicilio"> Parroquia</label>
-                        <select id="municipios" class="form form-control" name="parroquias">
-                          <option></option>
-                          <?php __ListarParroquias(); ?>
-                        </select>
+                        <input type="text" name="parroquia" required>
                     </div>
                     <h4 class="info"> Formación Académica</h4>
                     <div class="form-group">
                         <label for="correoElectronico"> Titulo obtenido</label>
-                        <select class="form-control" id="sel1" name="tituloObtenido">
+                        <select class="form-control" id="sel1" name="tituloObtenido" required>
                           <option value="0" onclick="javascript:nameformdiv.className='no';">-</option>
-                          <option value="1" onclick="javascript:nameformdiv.className='no';">Bachiller</option>
-                          <option value="2" onclick="javascript:nameformdiv.className='no';">Técnico Superior Universitario</option>
-                          <option value="3" onclick="javascript:nameformdiv.className='no';">Licenciatura</option>
-                          <option value="4" onclick="javascript:nameformdiv.className='no';">Ingeniería</option>
-                          <option value="5" onclick="javascript:nameformdiv.className='no';">Postgrado</option>
-                          <option value="6" onclick="javascript:nameformdiv.className='si';">Otros (Indique)</option>
+                          <option value="Bachiller" onclick="javascript:nameformdiv.className='no';">Bachiller</option>
+                          <option value="TecnicoSuperior" onclick="javascript:nameformdiv.className='no';">Técnico Superior Universitario</option>
+                          <option value="Licenciatura" onclick="javascript:nameformdiv.className='no';">Licenciatura</option>
+                          <option value="Ingenieria" onclick="javascript:nameformdiv.className='no';">Ingeniería</option>
+                          <option value="Postgrado" onclick="javascript:nameformdiv.className='no';">Postgrado</option>
+                          <option value="otro" onclick="javascript:nameformdiv.className='si';">Otros (Indique)</option>
                         </select>
                         <div id="nameformdiv" class="no" >
                         <p>Indique otro</p>
                         <p><input type="text" id="nameformdiv.name" class="input form-control" /></p>
                         </div>
                         <label for="correoElectronico"> Carrera</label>
-                        <input type="text" class="form-control" name="carrera" id="carrera" placeholder="Carrera">
+                        <input type="text" class="form-control" name="carrera" id="carrera" placeholder="Carrera" required>
                         <label for="correoElectronico"> Institución</label>
-                        <input type="text" class="form-control" name="institucion" id="institucion" placeholder="Institucion">
+                        <input type="text" class="form-control" name="institucion" id="institucion" placeholder="Institucion" required>
                     </div>
                     <h4 class="info"> Datos Laborales</h4>
                     <div class="form-group">
                         <label for="profesion"> Profesión/Ocupación</label>
-                        <input type="text" class="form-control" name="profesion" id="profesion" placeholder="Profesión/Ocupación">
+                        <input type="text" class="form-control" name="profesion" id="profesion" placeholder="Profesión/Ocupación" >
                         <label for="nombreEmpresa"> Nombre de la Empresa</label>
-                        <input type="text" class="form-control" name="nombreEmpresa" id="nombreEmpresa" placeholder="Nombre de la Empresa">
+                        <input type="text" class="form-control" name="nombreEmpresa" id="nombreEmpresa" placeholder="Nombre de la Empresa" >
                         <label for="direccionTrabajo"> Dirección</label>
-                        <input type="text" class="form-control" name="direccionTrabajo" id="direccionTrabajo" placeholder="Dirección">
+                        <input type="text" class="form-control" name="direccionTrabajo" id="direccionTrabajo" placeholder="Dirección" >
                     </div>
                     <button type="submit" class="btn btn-default btn-success btn-block" id="iniciar_sesion" name="button">Enviar</button>
 
